02e2930789120f7e9dc927512cd1d9d9fc309a62
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-05 22:38:15 -0700

    calculate mortgage rate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mortgage-calculator';
+
+  loan = new FormControl('');
+  interest = new FormControl('')
+  years = new FormControl('');
+  monthlyPayment: Number;
+
+  constructor(){
+    this.monthlyPayment = 0;
+  }
+
+  reCalculate(){
+    let loanNum = Number(this.loan.value);   
+    let interestNum = Number(this.interest.value);
+    let yearsNum = Number(this.years.value);
+
+    this.monthlyPayment = loanNum*((interestNum/12)*(1+(interestNum/12))*(yearsNum*12))/((1+(interestNum/12))*(yearsNum*12) - 1)
+  }
 }
+
+// M = P(i(1+i)n)/((1+i)n - 1)
+
+// M: Monthly mortgage payment
+// P: Loan amount
+// i: Monthly interest rate (APR / 12)
+// n: Total number of payments (loan term in years x 12)
